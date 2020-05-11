1d6e165aa60e443d25aae0d17d42045f20e5db47
Cody Heimberger <certifiedloud@gmail.com>
2020-05-10 22:29:21 -0500

    correct function name

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if [ $? = 0  ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 dotfiles checkout
 
