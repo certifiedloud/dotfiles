ead34aab4557000452e40f28de4300ca52440040
Cody Heimberger <certifiedloud@gmail.com>
2020-05-10 22:32:08 -0500

    just let it fail and have the user (me) decide what to do with existing files

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Ensure git is available
 command -v git >/dev/null 2>&1 || { echo >&2 "Please install git before continuing"; exit 1; }
@@ -12,14 +13,6 @@ function dotfiles {
 git clone --bare git@github.com:certifiedloud/dotfiles.git $HOME/.dotfiles
 
 # Checkout the content from the repo to $HOME
-mkdir -p .config-backup
-dotfiles checkout
-if [ $? = 0  ]; then
-  echo "Checked out config.";
-else
-  echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
 dotfiles checkout
 
 # Hide untracked files in this local repo
