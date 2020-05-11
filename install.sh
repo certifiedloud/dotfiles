2b9e905b0579b3e82b38794c5bfd191540e0bcaf
Cody Heimberger <certifiedloud@gmail.com>
2020-05-10 22:21:36 -0500

    line break mistake

--- install.sh
+++ install.sh
@@ -18,8 +18,7 @@ if [ $? = 0  ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs
-  -I{} mv {} .config-backup/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 dotfiles checkout
 
