931e8f4b44417febd8ccec48a2580c582d7c1bc0
Cody Heimberger <certifiedloud@gmail.com>
2020-05-10 22:14:52 -0500

    add function instead of alias to work within script scope

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -e
 
 
 # Ensure git is available
 command -v git >/dev/null 2>&1 || { echo >&2 "Please install git before continuing"; exit 1; }
 
 # Create the temp alias
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+function dotfiles {
+     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
 
 # Clone dotfiles
 git clone --bare git@github.com:certifiedloud/dotfiles.git $HOME/.dotfiles
