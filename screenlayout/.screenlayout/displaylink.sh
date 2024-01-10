04f1236def195e0a572b5a2d1dafcdba339545c2
Cody Heimberger <certifiedloud@gmail.com>
2024-01-09 20:35:09 -0600

    Removed outdated configs

--- screenlayout/.screenlayout/displaylink.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 4920x1000 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 3840x520 --rotate right --output HDMI-1 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-2 --off --output DP-1 --off
